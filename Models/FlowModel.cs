0f174d0f43078a5226ad1a443e41b1329b665cab
wiktorele <wiktor.ucinski@gmail.com>
2020-11-28 17:44:33 +0100

    Complete javascript criteria file

--- Models/FlowModel.cs
+++ Models/FlowModel.cs
@@ -14,7 +14,6 @@ namespace OnosFlow.Models
     }
     public class Flow
     {
-        //[Required]
         [RegularExpression("^[0-9]+")]
         [Display(Name = "Id przepływu")]
         public string id { get; set; }
@@ -47,7 +46,7 @@ namespace OnosFlow.Models
     }
     public class Treatment
     {
-        public Instruction[] instructions { get; set; }
+        public IList<Instruction> instructions { get; set; }
         public object[] deferred { get; set; }
         public bool clearDeferred { get; set; }
     }
@@ -56,14 +55,25 @@ namespace OnosFlow.Models
     {
         [Display(Name = "Typ instrukcji")]
         public string type { get; set; }
+        public string subtype { get; set; }
+        [Display(Name = "Port")]
         public string port { get; set; }
+        public string tableId { get; set; }
+        public string groupId { get; set; }
+        [Display(Name = "Adres mac")]
+        public string mac { get; set; }
+        [Display(Name = "Adres ip")]
+        public string ip { get; set; }
+        [Display(Name = "Port tcp")]
+        public string tcpPort { get; set; }
+        [Display(Name = "Port udp")]
+        public string udpPort { get; set; }
+
     }
 
     public class Selector
     {
         public IList<Criterion> criteria { get; set; }
-
-        //public ICollection<Criterion> criteria { get; set; }
     }
 
     public class Criterion
@@ -87,30 +97,8 @@ namespace OnosFlow.Models
 
         public static IEnumerable<string> GetTypes()
         {
-            return new[] { "ETH_TYPE", "ETH_DST", "ETH_SRC", "IPV4_SRC", "IPV4_DST", "IPV6_SRC", "IPV6_DST", "IN_PORT", "IP_PROTO" };
+            return new[] { "ETH_TYPE", "ETH_DST", "ETH_SRC", "IPV4_SRC", "IPV4_DST", "IPV6_SRC", "IPV6_DST", "IN_PORT", "IP_PROTO", "TCP_SRC", "TCP_DST", "UDP_SRC", "UDP_DST" };
         }
     }
-
-    //public class Criterion1
-    //{
-    //    public string type { get; set; }
-
-    //    public string eth_dst { get; set; }
-
-    //}
-    //public class Criterion2
-    //{
-    //    public string type { get; set; }
-
-    //    public string eth_src { get; set; }
-
-    //}
-
-    //public class CriterionType
-    //{
-    //    [Required]
-    //    public string Type { get; set; }
-    //}
-
 
 }
